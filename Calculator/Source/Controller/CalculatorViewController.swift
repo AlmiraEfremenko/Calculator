beb6298573305a3fecf647e1c14abc3ca228f215
AlmiraEfremenko <almira.efremenko@yandex.ru>
2023-01-31 21:25:14 +0500

    Create CalculatorViewController.

--- Calculator/Source/Controller/CalculatorViewController.swift
+++ Calculator/Source/Controller/CalculatorViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
+    let calculatorView = CalculatorView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
+        setupHierarchy()
+        setupLayout()
     }
 
+    private func setupHierarchy() {
+        view.addSubview(calculatorView)
+    }
 
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            calculatorView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            calculatorView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            calculatorView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10),
+            calculatorView.heightAnchor.constraint(equalTo: calculatorView.widthAnchor, multiplier: 1)
+        ])
+    }
 }
-
