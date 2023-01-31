f5b70e19bc171929a36f7b92b4fc8b21eb9e3b2f
AlmiraEfremenko <almira.efremenko@yandex.ru>
2023-01-31 21:24:46 +0500

    Create extension UIStackView.

--- Calculator/Source/UIKit/UIStackView+Ext.swift
+++ Calculator/Source/UIKit/UIStackView+Ext.swift
@@ -5,4 +5,14 @@
 //  Created by MAC on 22.01.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UIStackView {
+
+    convenience init(views: [UIView]) {
+        self.init(arrangedSubviews: views)
+        self.axis = .horizontal
+        self.distribution = .fillEqually
+        self.spacing = 1
+    }
+}
