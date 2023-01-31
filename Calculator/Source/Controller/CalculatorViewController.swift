82f39f4c44db2e40fe7279818d71c94beb81bfe1
AlmiraEfremenko <almira.efremenko@yandex.ru>
2023-01-31 21:43:41 +0500

    Add titleLabel.

--- Calculator/Source/Controller/CalculatorViewController.swift
+++ Calculator/Source/Controller/CalculatorViewController.swift
@@ -9,7 +9,18 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
-    let calculatorView = CalculatorView()
+    private let calculatorView = CalculatorView()
+
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "0"
+        label.font = .systemFont(ofSize: 60, weight: .bold)
+        label.textAlignment = .right
+        label.textColor = .white
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,11 +30,16 @@ class CalculatorViewController: UIViewController {
     }
 
     private func setupHierarchy() {
+        view.addSubview(titleLabel)
         view.addSubview(calculatorView)
     }
 
     private func setupLayout() {
         NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: calculatorView.topAnchor, constant: -10),
+
             calculatorView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             calculatorView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             calculatorView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10),
