957d0733fa63624c4ffa3a522f9ac708b6fb8f49
AlmiraEfremenko <almira.efremenko@yandex.ru>
2023-01-31 21:24:25 +0500

    Setup module.

---
+++ Calculator/Source/App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Calculator
+//
+//  Created by MAC on 22.01.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ Calculator/Source/UIKit/UIStackView+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  UIStackView+Ext.swift
+//  Calculator
+//
+//  Created by MAC on 22.01.2023.
+//
+
+import Foundation
